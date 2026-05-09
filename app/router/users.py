2380a901b5fa9a48866e5c4715828ace82e30f69
826663910 <826663910@qq.com>
2026-05-09 16:48:56 +0800

    111

--- app/router/users.py
+++ app/router/users.py
@@ -104,11 +104,12 @@ async def login(request: Request, db: AsyncSession = Depends(get_db),
     return {"msg": "登录成功"}
 
 
-@router.get('/user/{id}', response_model=schemas.UserOut)
-async def test(id: int, request: Request, db: AsyncSession = Depends(get_db),
+"""获取当前用户信息"""
+@router.get('/user/me', response_model=schemas.UserOut)
+async def test(request: Request, db: AsyncSession = Depends(get_db),
                current_user: dict = Depends(auth.check_login)):
     # 查询当前用户
-    stmt = select(models.User).where(models.User.id == id)
+    stmt = select(models.User).where(models.User.id == current_user.id)
     # 执行sql
     result = await db.execute(stmt)
     # 返回数据或者None
